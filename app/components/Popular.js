3e56a5661f1b3c9c77ce7ede1e5accf207315101
cmac1223 <chrismac539@gmail.com>
2018-06-25 12:39:23 -0400

    added event handler

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,13 +1,21 @@
 var React = require('react');
 
 class Popular extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
       selectedLanguage: 'All'
     }
   }
+
+  // event handler
+  updateLanguage(lang) {
+    this.setState(function () {
+      return { selectedLanguage: lang }
+    })
+  }
+
   render() {
     // the goal is to map over a list of langauges
     var langauges = ['All', 'Java', 'JavaScript', 'CSS', 'Python', 'Ruby'];
