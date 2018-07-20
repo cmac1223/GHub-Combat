47060c171803b01f839b9209bad0736fd95a0acc
cmac1223 <chrismac539@gmail.com>
2018-07-20 11:53:13 -0400

    more styling components

--- app/components/Home.js
+++ app/components/Home.js
@@ -4,7 +4,7 @@ var Link = require('react-router-dom').Link;
 class Home extends React.Component {
   render() {
     return (
-      <div>
+      <div className='home-container' > 
         <h1>Github Battle: Battle your friends...and stuff.</h1>
 
         <Link className='button' to='/battle' >
