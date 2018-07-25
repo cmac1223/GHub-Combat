96e05cd67e60378e915d576d16c871024515547d
cmac1223 <chrismac539@gmail.com>
2018-07-25 10:38:19 -0400

    added event handler and views to Battle Component

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -12,19 +12,35 @@ class Battle extends React.Component {
     }
   }
 
-  handleSubmit(id, username){
-    this.setState(function(){
+  handleSubmit(id, username) {
+    this.setState(function () {
       var newState = {};
       newState[id + 'Name'] = username;
-      newState[id + 'Image'] = 'https://github.com/'+username+'.png?size=200';
+      newState[id + 'Image'] = 'https://github.com/' + username + '.png?size=200';
       return newState;
     })
   }
 
   render() {
+    var playerOneName = this.state.playerOneName;
+    var playerTwoName = this.state.playerTwoName;
     return (
       <div>
-        Battle!
+        <div className='row' >
+          {!playerOneName &&
+            <PlayerInput
+              id='playerOne'
+              label='Player One'
+              onSubmit={this.handleSubmit}
+            />}
+
+          {!playerTwoName &&
+            <PlayerInput
+              id='playerOne'
+              label='Player Two'
+              onSubmit={this.handleSubmit}
+            />}
+        </div>
       </div>
     )
   }
