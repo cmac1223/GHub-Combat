6750afff4494bceea9d25f4d929a79b0f98901c4
cmac1223 <chrismac539@gmail.com>
2018-08-15 20:27:50 -0400

    add PlayerPreview Component to its on file replace <button/> with props.children}

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -2,32 +2,6 @@ var React = require('react');
 var PropTypes = require('prop-types');
 var Link = require('react-router-dom').Link;
 
-function PlayerPreview(props) {
-  return (
-    <div>
-      <div className='column' >
-        <img
-          className='avatar'
-          src={props.avatar}
-          alt={`Avatar for ${props.username}`}
-        />
-        <h2 className='username' >@{props.username}</h2>
-        <button
-          className='reset'
-          onClick={props.onReset.bind(null, props.id)}>
-          Reset
-        </button>
-      </div>
-    </div>
-  )
-}
-
-PlayerPreview.propTypes = {
-  avatar: PropTypes.string.isRequired,
-  username: PropTypes.string.isRequired,
-  id: PropTypes.string.isRequired,
-  onReset: PropTypes.func.isRequired,
-}
 
 class PlayerInput extends React.Component {
   constructor(props) {
@@ -142,9 +116,13 @@ class Battle extends React.Component {
             <PlayerPreview
               avatar={playerOneImage}
               username={playerOneName}
-              onReset={this.handleReset}
-              id='playerOne'
-            />}
+            >
+              <button
+                className='reset'
+                onClick={this.handleReset.bind(null, 'playerOne')}>
+                Reset
+              </button>
+            </PlayerPreview>}
 
           {!playerTwoName &&
             <PlayerInput
@@ -157,9 +135,13 @@ class Battle extends React.Component {
             <PlayerPreview
               avatar={playerTwoImage}
               username={playerTwoName}
-              onReset={this.handleReset}
-              id='playerTwo'
-            />}
+            >
+              <button
+                className='reset'
+                onClick={this.handleReset.bind(null, 'playerTwo')}>
+                Reset
+              </button>
+            </PlayerPreview>}
         </div>
         {playerOneImage && playerTwoImage &&
           <Link
